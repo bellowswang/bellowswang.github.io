7da00044199c0bfe20d096d04eb86b242f4949fe
Yihong Wang <bellowswang@gmail.com>
2023-06-13 21:37:51 +0200

    change config

--- eleventy.config.js
+++ eleventy.config.js
@@ -119,9 +119,9 @@ module.exports = function(eleventyConfig) {
 		// These are all optional:
 		dir: {
 			input: "content",          // default: "."
-			includes: "../_includes",  // default: "_includes"
-			data: "../_data",          // default: "_data"
-			output: "../_site"
+			includes: "_includes",  // default: "_includes"
+			data: "_data",          // default: "_data"
+			output: "_site"
 		},
 
 		// -----------------------------------------------------------------
